0543b2b503ae9c95789a6708ea726edb2ccc22b6
Student <s525078@ch140035m.nwmissouri.edu>
2016-03-17 09:55:42 -0500

    Added  2 files

---
+++ HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  someGitiOS
+//
+//  Created by Pandey,Ashish on 3/17/16.
+//  Copyright © 2016 Pandey,Ashish. All rights reserved.
+//
+
+import Cocoa
+
+class HomeViewController: UIViewController {
+
+}
---
+++ someGitiOS/HomeScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeScreenViewController.swift
+//  someGitiOS
+//
+//  Created by Pandey,Ashish on 3/17/16.
+//  Copyright © 2016 Pandey,Ashish. All rights reserved.
+//
+
+import Cocoa
+
+class HomeScreenViewController: UIViewController {
+
+}
