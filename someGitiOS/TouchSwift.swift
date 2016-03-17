7e817a59998638fb76447d8b2863457f5cb350a9
Student <s525078@ch140035m.nwmissouri.edu>
2016-03-17 10:13:46 -0500

    done	with class for touch demo

---
+++ someGitiOS/TouchSwift.swift
@@ -0,0 +1,15 @@
+//
+//  TouchSwift.swift
+//  someGitiOS
+//
+//  Created by Pandey,Ashish on 3/17/16.
+//  Copyright © 2016 Pandey,Ashish. All rights reserved.
+//
+
+import Foundation
+
+class DoNotTouch {
+  func saySomething(){
+        print("Please do touch swift once")
+    }
+}
